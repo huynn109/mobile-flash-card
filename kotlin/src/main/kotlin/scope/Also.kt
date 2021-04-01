9a1fe7d9a8d69c0770335950d008a51189cdd147
Huy Nguyen <huynn109@gmail.com>
2021-04-01 12:33:36 +0700

    Update scope function also

--- kotlin/src/main/kotlin/scope/Also.kt
+++ kotlin/src/main/kotlin/scope/Also.kt
@@ -15,7 +15,7 @@ fun changeAge(it: Person) {
  * Là extension của generic T
  * Nhận tham số truyền vào là anonymous function -> nên có thể sử dụng "it" để gọi T
  * Anonymous function có tham số là generic T và không có kiểu trả về
- * Kiểu trả về của Also chính là
+ * Kiểu trả về của Also chính là T
  * Thường dùng để check null của T đặc biệt khi T là tham số của hàm khác define tên sẽ rõ ràng hơn là this
  */
 fun <T> T.doAlso(block: (T) -> Unit): T {
