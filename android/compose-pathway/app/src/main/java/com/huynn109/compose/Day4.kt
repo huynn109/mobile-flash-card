e7f2bd5073dd5833c26d8c27f64bee950288b1be
Huy Nguyen <huynn109@gmail.com>
2021-07-21 16:23:31 +0700

    style day 4

--- android/compose-pathway/app/src/main/java/com/huynn109/compose/Day4.kt
+++ android/compose-pathway/app/src/main/java/com/huynn109/compose/Day4.kt
@@ -3,21 +3,22 @@ package com.huynn109.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.huynn109.compose.ui.theme.ComposeDayTheme
 
-class Day2 : ComponentActivity() {
+class Day4 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeDayTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PhotographerProfile()
                 }
             }
         }
@@ -25,14 +26,20 @@ class Day2 : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerProfile() {
+    Column {
+        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+        // LocalContentAlpha is defining opacity level of its children
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeDayTheme {
-        Greeting("Android")
+        PhotographerProfile()
     }
 }
