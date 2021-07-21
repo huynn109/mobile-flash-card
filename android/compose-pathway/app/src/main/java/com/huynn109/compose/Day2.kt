c0aee446d01d46080ab5c7c698c576d75962c380
Huy Nguyen <huynn109@gmail.com>
2021-07-21 15:24:26 +0700

    style gradle

---
+++ android/compose-pathway/app/src/main/java/com/huynn109/compose/Day2.kt
@@ -0,0 +1,38 @@
+package com.huynn109.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.huynn109.compose.ui.theme.ComposeDayTheme
+
+class Day2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeDayTheme {
+        Greeting("Android")
+    }
+}
