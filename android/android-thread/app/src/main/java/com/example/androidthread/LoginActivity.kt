81df46c0248031630bc72403929135605ceab27b
huynn109 <huynn109@gmail.com>
2022-04-08 09:13:55 +0700

    feat: thread

--- android/android-thread/app/src/main/java/com/example/androidthread/LoginActivity.kt
+++ android/android-thread/app/src/main/java/com/example/androidthread/LoginActivity.kt
@@ -1,10 +1,16 @@
 package com.example.androidthread
 
 import android.annotation.SuppressLint
-import android.os.*
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.Thread.currentThread
 
 
 class LoginActivity : AppCompatActivity() {
@@ -19,19 +25,49 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         findViewById<Button>(R.id.btnGetHandleMessage).setOnClickListener {
-           ThreadLooper().apply {
+            ThreadLooper(this).apply {
+                Log.d("ThreadLooper", "Message: ${Thread.currentThread().name}")
                 start()
                 for (i in 0 until 10) {
                     Thread.sleep(300)
-                    this.handler?.sendMessage(Message.obtain().apply { obj = "Send from UI thread $i" })
+                    this.handler?.sendMessage(
+                        Message.obtain().apply { obj = "Send from UI thread $i" })
                 }
             }
+//            val thread = PrimeThread(200, this)
+//            thread.start()
+//            val threadRunnable = PrimeThreadRunnable(200, this)
+//            Thread(threadRunnable).start()
         }
     }
 
 }
 
-class ThreadLooper : HandlerThread("Test-Thread") {
+class PrimeThread(var minPrime: Long, val context: Context) : Thread(), Runnable {
+    companion object {
+        const val TAG = "PrimeThread"
+    }
+
+    override fun run() {
+        Log.d(TAG, "run: ${currentThread().name}") // Background thread
+        Log.d(TAG, "run: $minPrime")
+//        Toast.makeText(context, currentThread().name, Toast.LENGTH_SHORT).show() // Crash: call UI from background thread
+    }
+}
+
+class PrimeThreadRunnable(var minPrime: Long, val context: Context) :Runnable {
+    companion object {
+        const val TAG = "PrimeThread"
+    }
+
+    override fun run() {
+        Log.d(TAG, "run: ${currentThread().name}") // Background thread
+        Log.d(TAG, "run: $minPrime")
+//        Toast.makeText(context, currentThread().name, Toast.LENGTH_SHORT).show() // Crash: call UI from background thread
+    }
+}
+
+class ThreadLooper(var context: Context) : Thread() {
     var handler: Handler? = null
     override fun run() {
         // Preparing looper it creates looper
@@ -41,8 +77,9 @@ class ThreadLooper : HandlerThread("Test-Thread") {
             @SuppressLint("HandlerLeak")
             object : Handler() {
                 override fun handleMessage(msg: Message) {
-                    Log.d("ThreadLooper", "handleMessage: ${msg.obj}")
-                    Log.d("ThreadLooper", "handleMessage: ${currentThread().name}")
+                    Log.d("ThreadLooper", "Message: ${msg.obj}")
+                    Log.d("ThreadLooper", "CurrentThread: ${currentThread().name}")
+//                    Toast.makeText(context, currentThread().name, Toast.LENGTH_SHORT).show()
                 }
             }
         //To start looping
